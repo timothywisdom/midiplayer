850fa0c1b7f8ec6efd3335d41f79a15aab5e4a89
timothywisdom <timothy@timothywisdom.com>
2018-09-11 07:38:47 -0700

    master formatting

--- src/components/webmidi/webmidi.tsx
+++ src/components/webmidi/webmidi.tsx
@@ -5,23 +5,21 @@ import * as React from 'react';
 import WebMidi from 'webmidi';
 
 class WebMidiProvider extends React.Component {
-    public render() {
-        return null;
-    }
+	public render() {
+		return null;
+	}
 
-    public componentDidMount() {
-        WebMidi.enable( (err: Error) => {
-
-            if (err) {
-                console.log("WebMidi could not be enabled.", err);
-            } else {
-                console.log("WebMidi enabled!");
-                console.log("Inputs", WebMidi.inputs);
-                console.log("Outputs", WebMidi.outputs);
-            }
-            
-          });
-    }
+	public componentDidMount() {
+		WebMidi.enable((err: Error) => {
+			if (err) {
+				console.log('WebMidi could not be enabled.', err);
+			} else {
+				console.log('WebMidi enabled!');
+				console.log('Inputs', WebMidi.inputs);
+				console.log('Outputs', WebMidi.outputs);
+			}
+		});
+	}
 }
 
 export default WebMidiProvider;
